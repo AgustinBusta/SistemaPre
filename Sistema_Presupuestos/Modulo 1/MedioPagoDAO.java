e376b5fee3dc4f84485969f7fa6fe2b42ce31862
AgustinBusta <agusb685@gmail.com>
2025-07-30 15:34:10 -0300

    Delete Sistema_Presupuestos/Modulo 1 directory

--- Sistema_Presupuestos/Modulo 1/MedioPagoDAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-import java.util.Optional;
-
-public interface MedioPagoDAO {
-    List<MedioPago> obtenerTodos();
-    Optional<MedioPago> obtenerPorId(int id);
-    boolean guardar(MedioPago medioPago);
-    boolean desactivar(int id);
-}
